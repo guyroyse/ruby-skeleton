d778002378460ee4e8c208c9e4a034e682554d9c
groyse <GuyRoyse@dswinc.com>
2013-10-25 10:08:51 -0400

    displays value of a nickel

--- lib/vending_machine.rb
+++ lib/vending_machine.rb
@@ -1,7 +1,15 @@
 class VendingMachine
+  
+  def initialize
+    @display = 'INSERT COIN'
+  end
 
   def display
-    'INSERT COIN'
+    @display
+  end
+
+  def insert coin
+    @display = '0.05'
   end
 
 end
